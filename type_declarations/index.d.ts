15d6cd11dafb9682d442b680cb7c1aebd5d61b9d
Christopher Brown <io@henrian.com>
2015-11-24 00:10:49 -0600

    Replace type_declarations/ with declarations package

--- type_declarations/index.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="DefinitelyTyped/angularjs/angular.d.ts" />
-/// <reference path="DefinitelyTyped/angularjs/angular-resource.d.ts" />
-/// <reference path="DefinitelyTyped/lodash/lodash.d.ts" />
